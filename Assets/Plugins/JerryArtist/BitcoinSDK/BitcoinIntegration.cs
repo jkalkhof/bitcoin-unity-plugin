fd1192a1067a134073e81919abdfb555d5742a30
homeuser <bogus@yahoo.com>
2017-12-07 16:39:43 -0800

    cleanup scaffolding - broken function calls

--- Assets/Plugins/JerryArtist/BitcoinSDK/BitcoinIntegration.cs
+++ Assets/Plugins/JerryArtist/BitcoinSDK/BitcoinIntegration.cs
@@ -103,15 +103,6 @@ public class BitcoinIntegration : MonoBehaviour {
             {
                 using (var currentActivity = javaUnityPlayer.GetStatic<AndroidJavaObject>("currentActivity"))
                 {
-                    //using (var androidPlugin = new AndroidJavaObject("de.schildbach.wallet.integration.android.BitcoinIntegration", currentActivity))
-					//using (var androidPlugin = new AndroidJavaObject("com.jerryartist.unity_android_btc.BitcoinIntegration", currentActivity))
-					
-					
-					// v2 - use class, then call?
-					// var androidPluginClass = new AndroidJavaClass("de.schildbach.wallet.integration.android.BitcoinIntegration");
-					// using (var androidPlugin = androidPluginClass.Call<AndroidJavaObject>("de.schildbach.wallet.integration.android.BitcoinIntegration", currentActivity))
-					// error: java.lang.NoSuchMethodError: no non-static method with name='de.schildbach.wallet.integration.android.BitcoinIntegration' signature='(Lcom.unity3d.player.UnityPlayerActivity;)Ljava/lang/Object;' in class Ljava.lang.Object;
-                    
 					using (var androidPlugin = new AndroidJavaObject("de.schildbach.wallet.integration.android.BitcoinIntegration", currentActivity))                          
                     {
 						AndroidJavaObject address0 = new AndroidJavaObject("java.lang.String", sendToWalletAddress);
@@ -131,97 +122,6 @@ public class BitcoinIntegration : MonoBehaviour {
 
                         AndroidJavaObject memoStr = new AndroidJavaObject("java.lang.String", transactionNotes);
 
-
-                        //						jobjectArray arr = env->NewObjectArray( 5, env->FindClass( "java/lang/String" ), env->NewStringUTF( "" ) );
-                        //    					const char *message[5]= { "first", "second", "third", "fourth", "fifth" }
-
-                        //androidPlugin.Call("handleRequestUnity", currentActivity, address0, address1, memoStr);
-
-                        //androidPlugin.Call("handleRequestUnity", currentActivity, address0, address1, amount0, amount1, memoStr);
-
-                        // name='handleRequestUnity'
-                        // signature='(
-                        //	Lcom.unity3d.player.UnityPlayerActivity;
-                        //	Ljava.lang.String;
-                        //	Ljava.lang.String;
-                        //	Ljava.lang.Long;
-                        //	Ljava.lang.Long;
-                        //	Ljava.lang.String;)V'
-
-                        /*
-				         // to get the activity
-				         //var androidJC = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-						var androidJC = new AndroidJavaClass("android.app.Activity");
-				        var jo = androidJC.GetStatic<AndroidJavaObject>("currentActivity");
-
-						// E/Unity: AndroidJavaException: java.lang.NoSuchFieldError: no "Ljava/lang/Object;" field "currentActivity" in class "Landroid/app/Activity;" or its superclasses
-						//	java.lang.NoSuchFieldError: no "Ljava/lang/Object;" field "currentActivity" in class "Landroid/app/Activity;" or its superclasses
-						*/
-
-                        /*
-						// try to cast to android.app.Activity
-						//var serviceObj = new AndroidJavaObject("android.app.Activity", currentActivity);
-						//var jo = Cast(currentActivity, "android.App.Activity");
-						string destClass = "android.app.Activity";
-
-						Debug.Log("BitcoinIntegration: ClassForName: " + destClass);
-
-	    				using (var clazz = new AndroidJavaClass("java.lang.Class"))
-	    				{
-							var destClassAJC = clazz.CallStatic<AndroidJavaObject>("forName", destClass);
-
-							var jo = destClassAJC.Call<AndroidJavaObject>("cast", currentActivity);
-	
-							object[] args  = new object[6];
-							args[0] = jo;
-							args[1] = address0;
-							args[2] = address1;
-							args[3] = amount0;
-							args[4] = amount1;
-							args[5] = memoStr;
-	
-							androidPlugin.Call("handleRequestUnity", args);
-					    }
-						*/
-
-                        /*
-						// try to cast to "de.schildbach.wallet.integration.android.BitcoinIntegration"
-						string destClass = "de.schildbach.wallet.integration.android.BitcoinIntegration";
-						Debug.Log("BitcoinIntegration: ClassForName: " + destClass);
-	    				using (var clazz = new AndroidJavaClass("java.lang.Class"))
-	    				{
-							var destClassAJC = clazz.CallStatic<AndroidJavaObject>("forName", destClass);
-
-							//var jo = destClassAJC.Call<AndroidJavaObject>("cast", androidPlugin);
-							var jo = destClassAJC.Call<AndroidJavaObject>("cast", androidPlugin);
-	
-							object[] args  = new object[6];
-							args[0] = address0;
-							args[1] = address1;
-							args[2] = amount0;
-							args[3] = amount1;
-							args[4] = memoStr;
-	
-							jo.Call("handleRequestUnity", args);
-					    }
-						*/
-
-                        // run a non-static method
-                        //currentActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>                                                                   {
-                        //              		//currentActivity.Call("testMethod", "testString");
-                        //	object[] args  = new object[5];						
-                        //	args[0] = address0;
-                        //	args[1] = address1;
-                        //	args[2] = amount0;
-                        //	args[3] = amount1;
-                        //	args[4] = memoStr;
-
-                        //	androidPlugin.Call("handleRequestUnity", args);
-                        //     				 }));
-                        // error:
-                        // JNI ERROR (app bug): accessed deleted global reference 0x1006a2
-
-
                         // generic call
                         object[] args = new object[5];
                         args[0] = address0;
@@ -231,10 +131,7 @@ public class BitcoinIntegration : MonoBehaviour {
                         args[4] = memoStr;
 
                         androidPlugin.Call("handleRequestUnity", args);
-                        // v1 error: 
-                        // E/Unity: AndroidJavaException: java.lang.NoSuchMethodError: no non-static method with name='handleRequestUnity' signature='(Ljava.lang.String;Ljava.lang.String;Ljava.lang.Long;Ljava.lang.Long;Ljava.lang.String;)V' in class Ljava.lang.Object;
-                        // no non-static method with name='handleRequestUnity' signature='(Ljava.lang.String;Ljava.lang.String;Ljava.lang.Long;Ljava.lang.Long;Ljava.lang.String;)V'
-                        //androidPlugin.Call("handleRequest");
+
 
                     }
                 }
