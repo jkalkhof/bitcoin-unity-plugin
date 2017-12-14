c21f694b8181c59a3154126de556799e3898b98b
homeuser <bogus@yahoo.com>
2017-12-13 17:54:58 -0800

    verify purchase of  multiple assets in the simple gui

--- Assets/Plugins/JerryArtist/BitcoinSDK/BitcoinSDKSimpleGui.cs
+++ Assets/Plugins/JerryArtist/BitcoinSDK/BitcoinSDKSimpleGui.cs
@@ -5,29 +5,66 @@ using UnityEngine.UI;
 
 public class BitcoinSDKSimpleGui : MonoBehaviour {
 
-    public GameObject Button_Purchase1;
-    public GameObject Button_Verify1;
-    public GameObject InputField_Transaction1;
+    //public GameObject Button_Purchase1;
+    //public GameObject Button_Verify1;
+    //public GameObject InputField_Transaction1;
+    //public GameObject RawImage_Lock1;
 
     public GameObject LoggingText;
 
     public Texture lockTexture;
     public Texture unlockTexture;
-    public GameObject RawImage_Lock1;
+    
 
-    private string transactionNotes = "Sample transaction from bitcoin sdk";
-    private string currencyTypeStr = "BTC - satoshis";
-    //private string currencyAmountStr = "12903";
-    private string currencyAmountStr = "300000"; // in satoshis
+    [System.Serializable]
+    public class BitcoinPurchaseObjectGroup
+    {
+        public GameObject Button_Purchase1;
+        public GameObject Button_Verify1;
+        public GameObject InputField_Transaction1;
+        public GameObject RawImage_Lock1;
+    }
 
-    private string sourceCurrencyTypeStr = "USD";
-    private string sourceCurrencyAmountStr = "1";
 
-    //private string sendToWalletAddress = "1GsBUQCNLdphxhuX6aZ7QAJjpnMq8MF6p8"; // jerry's donation wallet address 4
-    private string sendToWalletAddress = "1C53cU1oqmqwco38ZawdVQqVemaqa7aWQi"; // jerry's mobile wallet address 2
+    public BitcoinPurchaseObjectGroup[] objectGroups;
 
-    private string transactionHashStr = "10990f1892354ac9f11d634332041f2616e31ceb87fbc9077ac615cdf22c0d51"; // test transaction
-    //private string transactionHashStr = "";
+
+    public class BitcoinAsset
+    {
+        public string transactionNotes = "Sample transaction from bitcoin sdk";
+        public string currencyTypeStr = "satoshis";
+        public string currencyAmountStr = "0"; // in satoshis
+        public string sourceCurrencyTypeStr = "USD";
+        public string sourceCurrencyAmountStr = "1";
+        public string sendToWalletAddress = "";
+        public string transactionHashStr = ""; // test transaction                                                                                                                
+    }
+
+    //Array<BitcoinAsset> availableAssets = new Array<BitcoinAsset>()
+    BitcoinAsset[] availableAssets =
+    {
+        new BitcoinAsset{
+            transactionNotes = "Asset 1 purchase",
+            currencyTypeStr = "satoshis",
+            currencyAmountStr = "300000", // in satoshis - overridden by sourceCurrencyAmount when tryping to purchase
+            sourceCurrencyTypeStr = "USD",
+            sourceCurrencyAmountStr = "1",
+            sendToWalletAddress = "1C53cU1oqmqwco38ZawdVQqVemaqa7aWQi", // jerry's mobile wallet address 2
+            transactionHashStr = "10990f1892354ac9f11d634332041f2616e31ceb87fbc9077ac615cdf22c0d51" // test transaction validation
+        },
+        new BitcoinAsset{
+            transactionNotes = "Asset 2 purchase",
+            currencyTypeStr = "satoshis",
+            currencyAmountStr = "0", // in satoshis - overridden by sourceCurrencyAmount when tryping to purchase
+            sourceCurrencyTypeStr = "USD",
+            sourceCurrencyAmountStr = "1",
+            sendToWalletAddress = "1GsBUQCNLdphxhuX6aZ7QAJjpnMq8MF6p8", // jerry's donation wallet address 4
+            transactionHashStr = "" // test transaction validation
+        },
+    };
+
+    int currentBtcAsset = 0;
+
 
     private int debugLevel = 1;
 
@@ -47,10 +84,11 @@ public class BitcoinSDKSimpleGui : MonoBehaviour {
 	}
 
     // start purchase sequence
-    public void purchase1()
+    public void purchase(int index)
     {
-        if (debugLevel > 0) Debug.Log("BitcoinSDKGui: purchase1");
+        if (debugLevel > 0) Debug.Log("BitcoinSDKGui: purchase " + index);
 
+        currentBtcAsset = index;
         BlockchainWebChecker.instance.GetExchangeRates(GetExchangeRatesCallback);
 
     }
@@ -66,18 +104,18 @@ public class BitcoinSDKSimpleGui : MonoBehaviour {
         }
 
         // convert currency to BTC
-        float sourceCurrencyAmount = float.Parse(sourceCurrencyAmountStr);
-        float targetBtc = BlockchainWebChecker.instance.dataMgr.convertCurrencyToBtc(sourceCurrencyAmount, "USD");
+        float sourceCurrencyAmount = float.Parse(availableAssets[currentBtcAsset].sourceCurrencyAmountStr);
+        float targetBtc = BlockchainWebChecker.instance.dataMgr.convertCurrencyToBtc(sourceCurrencyAmount, availableAssets[currentBtcAsset].sourceCurrencyTypeStr);
 
         float targetSatoshi = targetBtc * satoshiToBtc;
         int targetSatoshiInt = Mathf.RoundToInt(targetSatoshi);
 
         Debug.Log("BitcoinSDKGui: GetExchangeRatesCallback: " +
-            sourceCurrencyAmount + " USD => " + targetBtc + " btc => (" + targetSatoshi + ") " + targetSatoshiInt + " satoshi");
+            sourceCurrencyAmount + " " + availableAssets[currentBtcAsset].sourceCurrencyTypeStr + " => " + targetBtc + " btc => (" + targetSatoshi + ") " + targetSatoshiInt + " satoshi");
         messageLog += sourceCurrencyAmount.ToString() + " USD => " + targetBtc + " btc => (" + targetSatoshi + ") " + targetSatoshiInt + " satoshi";
 
 
-        currencyAmountStr = targetSatoshiInt.ToString();      
+        availableAssets[currentBtcAsset].currencyAmountStr = targetSatoshiInt.ToString();      
 
         LoggingText.GetComponent<Text>().text = messageLog;
 
@@ -87,8 +125,8 @@ public class BitcoinSDKSimpleGui : MonoBehaviour {
     // request btc transfer
     private void requestMoney()
     {
-        long amount = long.Parse(currencyAmountStr);
-        BitcoinIntegration.instance.StartRequest(sendToWalletAddress, amount, transactionNotes, SendRequestCallback);
+        long amount = long.Parse(availableAssets[currentBtcAsset].currencyAmountStr);
+        BitcoinIntegration.instance.StartRequest(availableAssets[currentBtcAsset].sendToWalletAddress, amount, availableAssets[currentBtcAsset].transactionNotes, SendRequestCallback);
     }
 
     // request btc transfer - callback
@@ -98,21 +136,21 @@ public class BitcoinSDKSimpleGui : MonoBehaviour {
         messageLog += "SendRequestCallback\n";
         messageLog += data.message;
 
-        transactionHashStr = BitcoinIntegration.instance.getCurrentTxHash();
-        InputField_Transaction1.GetComponentInChildren<InputField>().text = transactionHashStr;
+        availableAssets[currentBtcAsset].transactionHashStr = BitcoinIntegration.instance.getCurrentTxHash();
+        objectGroups[currentBtcAsset].InputField_Transaction1.GetComponentInChildren<InputField>().text = availableAssets[currentBtcAsset].transactionHashStr;
 
         LoggingText.GetComponent<Text>().text = messageLog;
 
-        if ((transactionHashStr != null) && (transactionHashStr.Length > 0))
+        if ((availableAssets[currentBtcAsset].transactionHashStr != null) && (availableAssets[currentBtcAsset].transactionHashStr.Length > 0))
         {
-            verifyTransactions();
+            verifyTransactions(currentBtcAsset);
         }
     }
 
-    public void verifyTransactions()
+    public void verifyTransactions(int index)
     {
-        
-        StartCoroutine(BlockchainWebChecker.GetTransactionInfo_Coroutine(transactionHashStr, GetTXHashCallback));
+        currentBtcAsset = index;
+        StartCoroutine(BlockchainWebChecker.GetTransactionInfo_Coroutine(availableAssets[currentBtcAsset].transactionHashStr, GetTXHashCallback));
 
     }
 
@@ -127,18 +165,18 @@ public class BitcoinSDKSimpleGui : MonoBehaviour {
             messageLog += data.message;
         }
 
-        long amount = long.Parse(currencyAmountStr);
+        long amount = long.Parse(availableAssets[currentBtcAsset].currencyAmountStr);
 
-        bool verifyTest = BlockchainWebChecker.instance.dataMgr.verifyTransaction(sendToWalletAddress, amount);
+        bool verifyTest = BlockchainWebChecker.instance.dataMgr.verifyTransaction(availableAssets[currentBtcAsset].sendToWalletAddress, amount);
         if (verifyTest)
         {
             messageLog += "verified " + amount + "\n";
-            RawImage_Lock1.GetComponentInChildren<RawImage>().texture = unlockTexture;
+            objectGroups[currentBtcAsset].RawImage_Lock1.GetComponentInChildren<RawImage>().texture = unlockTexture;
         }
         else
         {
             messageLog += "verify failed! \n";
-            RawImage_Lock1.GetComponentInChildren<RawImage>().texture = lockTexture;
+            objectGroups[currentBtcAsset].RawImage_Lock1.GetComponentInChildren<RawImage>().texture = lockTexture;
         }
 
         LoggingText.GetComponent<Text>().text = messageLog;
